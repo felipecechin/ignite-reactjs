06033a3c9c914dba947fcd592718b2a59506cd8e
Felipe <ficechin@hotmail.com>
2022-03-27 20:47:48 -0300

    second module concluded

--- 01-github-explorer/src/App.jsx
+++ 01-github-explorer/src/App.jsx
@@ -1,4 +1,11 @@
+import { Counter } from './components/Counter'
+import { RepositoryList } from './components/RepositoryList'
 import './styles/global.scss'
 export function App() {
-    return <h1>Hello world aeaa</h1>
+    return (
+        <>
+            <RepositoryList />
+            <Counter />
+        </>
+    )
 }
