eff9a0ff84acc97a60bedaccd198e28127769c19
Felipe Cechin <ficechin@gmail.com>
2022-04-30 21:25:27 -0300

    signin with faunadb

--- ignews/src/pages/api/auth/[...nextauth].ts
+++ ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from "faunadb"
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from '../../../services/fauna'
+
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -7,12 +11,25 @@ export default NextAuth({
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-	  authorization: {
-		  params: {
-			  scope: 'read:user'
-		  }
-	  }
-    }),
-    // ...add more providers here
+      authorization: {
+        params: {
+          scope: 'read:user'
+        }
+      }
+    })
   ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+
+      const { email: userEmail } = user
+      await fauna.query(
+        q.Create(
+          q.Collection('users'),
+          { data: { email: userEmail } }
+        )
+      )
+
+      return true;
+    }
+  }
 })
