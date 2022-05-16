99d54c23d4f6739f45aa8d1b909685df9c2f559a
Felipe Cechin <ficechin@gmail.com>
2022-05-16 00:32:00 -0300

    posts loading

--- ignews/src/pages/posts/index.tsx
+++ ignews/src/pages/posts/index.tsx
@@ -3,8 +3,21 @@ import Head from 'next/head';
 import { createClient } from '../../../prismicio';
 import styles from './styles.module.scss';
 import * as prismic from '@prismicio/client'
+import * as prismicH from '@prismicio/helpers'
+import Link from 'next/link';
 
-export default function Posts() {
+type Post = {
+    slug: string;
+    title: string;
+    excerpt: string;
+    updatedAt: string;
+}
+
+interface PostsProps {
+    posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
     return (
         <>
             <Head>
@@ -15,21 +28,15 @@ export default function Posts() {
 
             <main className={styles.container}>
                 <div className={styles.posts}>
-                    <a href='#'>
-                        <time>12 de março de 2021</time>
-                        <strong>Creating a Monorepo with </strong>
-                        <p>In the guide...</p>
-                    </a>
-                    <a href='#'>
-                        <time>12 de março de 2021</time>
-                        <strong>Creating a Monorepo with </strong>
-                        <p>In the guide...</p>
-                    </a>
-                    <a href='#'>
-                        <time>12 de março de 2021</time>
-                        <strong>Creating a Monorepo with </strong>
-                        <p>In the guide...</p>
-                    </a>
+                    {posts.map(post => (
+                        <Link key={post.slug} href={`/posts/${post.slug}`}>
+                            <a>
+                                <time>{post.updatedAt}</time>
+                                <strong>{post.title}</strong>
+                                <p>{post.excerpt}</p>
+                            </a>
+                        </Link>
+                    ))}
                 </div>
             </main>
         </>
@@ -47,8 +54,22 @@ export const getStaticProps: GetStaticProps = async ({ previewData }) => {
         pageSize: 100
     })
 
-    console.log(JSON.stringify(response, null, 2));
+    const posts = response.results.map(post => {
+        return {
+            slug: post.uid,
+            title: prismicH.asText(post.data.title),
+            excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+            updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric'
+            })
+        }
+    })
+
     return {
-        props: {}
+        props: {
+            posts
+        }
     }
 }
