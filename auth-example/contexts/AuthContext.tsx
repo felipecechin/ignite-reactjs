74a3b7e33c414c9e5819dcf87d97c535d7d312ff
Felipe Cechin <ficechin@gmail.com>
2022-06-27 23:52:26 -0300

    refresh token developing

--- auth-example/contexts/AuthContext.tsx
+++ auth-example/contexts/AuthContext.tsx
@@ -1,4 +1,6 @@
-import { createContext, ReactNode, useContext } from "react";
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import { setCookie, parseCookies } from 'nookies'
+import Router from "next/router";
 import { api } from "../services/api";
 
 type SignInCredentials = {
@@ -6,8 +8,15 @@ type SignInCredentials = {
     password: string;
 }
 
+type User = {
+    email: string;
+    permissions: string[];
+    roles: string[];
+}
+
 type AuthContextData = {
     signIn(credentials: SignInCredentials): Promise<void>;
+    user: User;
     isAuthenticated: boolean;
 }
 
@@ -18,20 +27,52 @@ type AuthProviderProps = {
 const AuthContext = createContext({} as AuthContextData);
 
 export function AuthProvider({ children }: AuthProviderProps) {
-    const isAuthenticated = false;
+    const [user, setUser] = useState<User>();
+    const isAuthenticated = !!user;
+
+    useEffect(() => {
+        const { 'nextauth.token': token } = parseCookies()
+        if (token) {
+            api.get('/me').then(response => {
+                const { email, permissions, roles } = response.data
+
+                setUser({ email, permissions, roles })
+            })
+        }
+    }, [])
 
     async function signIn({ email, password }: SignInCredentials) {
         try {
             const response = await api.post('sessions', { email: email, password: password })
 
-            console.log(response.data)
+            const { token, refreshToken, permissions, roles } = response.data;
+
+            setCookie(undefined, 'nextauth.token', token, {
+                maxAge: 60 * 60 * 24 * 30, // 30 days
+                path: '/'
+            })
+
+            setCookie(undefined, 'nextauth.refreshToken', refreshToken, {
+                maxAge: 60 * 60 * 24 * 30, // 30 days
+                path: '/'
+            })
+
+            setUser({
+                email,
+                permissions,
+                roles
+            })
+
+            api.defaults.headers['Authorization'] = `Bearer ${token}`
+
+            Router.push('/dashboard')
         } catch (err) {
             console.log(err)
         }
     }
 
     return (
-        <AuthContext.Provider value={{ signIn, isAuthenticated }}>
+        <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
             {children}
         </AuthContext.Provider>
     )
--- auth-example/services/api.ts
+++ auth-example/services/api.ts
@@ -1,5 +1,46 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+import { parseCookies, setCookie } from "nookies";
+
+let cookies = parseCookies()
+
 
 export const api = axios.create({
-    baseURL: "http://localhost:3333"
+    baseURL: "http://localhost:3333",
+    headers: {
+        Authorization: `Bearer ${cookies['nextauth.token']}`
+    }
+})
+
+api.interceptors.response.use(response => {
+    return response
+}, (error: AxiosError) => {
+    if (error.response.status === 401) {
+        console.log("deu erro")
+        console.log(error.response)
+        if ((error.response.data as any).code === 'token.expired') {
+            cookies = parseCookies();
+
+            const { 'nextauth.refreshToken': refreshToken } = cookies;
+
+            api.post('/refresh', {
+                refreshToken,
+            }).then(response => {
+                const { token } = response.data;
+
+                setCookie(undefined, 'nextauth.token', token, {
+                    maxAge: 60 * 60 * 24 * 30, // 30 days
+                    path: '/'
+                })
+
+                setCookie(undefined, 'nextauth.refreshToken', response.data.refreshToken, {
+                    maxAge: 60 * 60 * 24 * 30, // 30 days
+                    path: '/'
+                })
+
+                api.defaults.headers['Authorization'] = `Bearer ${token}`
+            })
+        } else {
+
+        }
+    }
 })
